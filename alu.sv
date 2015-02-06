a32d0b924731ab048d4380ce0a2f318c7b2454ee
Lewis Russell <lewis6991@gmail.com>
2015-02-06 11:11:46 +0000

    Create alu.sv

---
+++ alu.sv
@@ -0,0 +1,13 @@
+//----------------------------------------
+// File: alu.sv
+// Description: Arithmetic Logic Unit
+//----------------------------------------
+module alu(
+    input  logic [31:0] A   ,
+    input  logic [31:0] B   ,
+    output logic [31:0] Out ,
+    input  logic [ 5:0] Func,
+);
+
+
+endmoudule
