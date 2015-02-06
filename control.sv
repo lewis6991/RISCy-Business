71926e10e085902231c2d28bc70976bc510ff85c
Lewis Russell <lewis6991@gmail.com>
2015-02-06 11:14:09 +0000

    Create control.sv

---
+++ control.sv
@@ -0,0 +1,11 @@
+//---------------------------------------------
+// File: Control.sv
+// Description: Control module.
+//---------------------------------------------
+
+module control(
+    input logic Clock,
+    input logic nReset,
+);
+
+endmodule
