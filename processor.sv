96d903bc3fb526f1de94ddb73d181b77e999aff2
Lewis Russell <lewis6991@gmail.com>
2015-02-06 11:16:38 +0000

    Create processor.sv

---
+++ processor.sv
@@ -0,0 +1,11 @@
+//-----------------------------------------------------
+// File: processor.sv
+// Description: Top-level module for MIPS32 processor.
+//-----------------------------------------------------
+
+module processor(
+    input logic Clock,
+    input logic nReset
+);
+
+endmodule
