c0a90bbb1c46070562fc82e9db5e4178478c16e9
Lewis Russell <lewis6991@gmail.com>
2015-02-06 11:19:30 +0000

    Create registers.sv

---
+++ registers.sv
@@ -0,0 +1,11 @@
+//------------------------------------------------
+// File: registers.sv
+// Description: Register file module of 32x32-bit registers.
+//------------------------------------------------
+
+module registers(
+
+);
+
+
+endmodule
