a0c0156e9a180dab80a9f34911061e1f5883a818
jwb1g11 <jwb1g11@soton.ac.uk>
2015-02-10 15:40:11 +0000

    Create WB.sv

---
+++ sv/test/WB.sv
@@ -0,0 +1,26 @@
+//----------------------------------------
+// File: WB_tb.sv
+// Description: Write Back testbench
+// Primary Author: Jack Barnes
+// Other Contributors:
+// Notes: Full test coverage
+//----------------------------------------
+module WB_tb;
+
+timeunit 10ns; timeprecision 100ps;
+const int clk = 100;
+
+WB WB0 (
+);
+
+//Initial conditions
+initial
+begin
+end
+
+//Testing procedure
+initial
+begin
+end
+
+endmodule
