7222dc7393a94ccff6aa094cc9ffbbba56cbf513
Dominic Murphy <dm16g11@soton.ac.uk>
2015-02-10 11:09:21 +0000

    Changed tabs to spaces.

--- sv/src/mul_definition.sv
+++ sv/src/mul_definition.sv
@@ -6,11 +6,11 @@
 // Notes: 
 //----------------------------------------
 
-`define		CLO		6'b100001
-`define		CLZ		6'b100000
+`define     CLO     6'b100001
+`define     CLZ     6'b100000
 
-`define		MADD	6'b000000
-`define		MADDU	6'b000001
-`define		MSUB	6'b000100
-`define		MSUBU	6'b000101
-`define		MUL		6'b000010	
+`define     MADD    6'b000000
+`define     MADDU   6'b000001
+`define     MSUB    6'b000100
+`define     MSUBU   6'b000101
+`define     MUL     6'b000010   
