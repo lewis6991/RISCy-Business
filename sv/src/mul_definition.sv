f273f8191e62e22517d13d54e2ed5b5a28c61f5f
dhanu1907 <dr3g11@soton.ac.uk>
2015-02-10 11:00:41 +0000

    Definition Files

---
+++ sv/src/mul_definition.sv
@@ -0,0 +1,16 @@
+//----------------------------------------
+// File: mul_definition.sv
+// Description: MUL instruction codes
+// Primary Author: Dhanushan
+// Other Contributors: 
+// Notes: 
+//----------------------------------------
+
+`define		CLO		6'b100001
+`define		CLZ		6'b100000
+
+`define		MADD	6'b000000
+`define		MADDU	6'b000001
+`define		MSUB	6'b000100
+`define		MSUBU	6'b000101
+`define		MUL		6'b000010	
