05948cab7cbaab7c8c0c26a4640998c69c81fcfe
Dominic Murphy <dm16g11@soton.ac.uk>
2015-02-11 17:05:51 +0000

    Updated function names

--- sv/test/RUNME.sh
+++ sv/test/RUNME.sh
@@ -13,11 +13,11 @@ main()
     case $1 in
         (-h|--help) usage;;
         (-a|--all)  all;;
-        (-if)       fet;;
-        (-id)       dec;;
-        (-exe)      exe;;
-        (-mem)      mem;;
-        (-wb)       wrb;;
+        (-if)       IF;;
+        (-id)       ID;;
+        (-exe)      EX;;
+        (-mem)      MEM;;
+        (-wb)       WB;;
         (-*)        echo "Unknown argument. Try --help for information";;
         (*)         echo "No argument given. Try --help for information";;
     esac
@@ -30,7 +30,7 @@ usage()
     -a,--all      Runs all test conditions
     -if           Runs the instruction fetch tests
     -id           Runs the instruction decode tests
-    -exe          Runs the execute tests
+    -ex           Runs the execute tests
     -mem          Runs the memory tests
     -wb           Runs the write back tests
     "
@@ -46,7 +46,7 @@ all()
     wrb
 }
 
-fet()
+IF()
 {
     ncverilog  -sv  -q  +nctimescale+1ns/10ps mux_tb.sv       ../src/mux.sv
     ncverilog  -sv  -q  +nctimescale+1ns/10ps pcinc_tb.sv     ../src/pcinc.sv
@@ -54,25 +54,25 @@ fet()
     ncverilog  -sv  -q  +nctimescale+1ns/10ps stages/IF_tb.sv ../src/IF.sv
 }
 
-dec()
+ID()
 {
     ncverilog  -sv  -q  +nctimescale+1ns/10ps registers_tb.sv        ../src/registers.sv
     ncverilog  -sv  -q  +nctimescale+1ns/10ps nleftshift_tb.sv ../src/nleftshift.sv
     ncverilog  -sv  -q  +nctimescale+1ns/10ps signextend_tb.sv ../src/signextend.sv
 }
 
-exe()
+EX()
 {
     ncverilog  -sv  -q  +nctimescale+1ns/10ps mux_tb.sv       ../src/mux.sv
     ncverilog  -sv  -q  +nctimescale+1ns/10ps alu_tb.sv       ../src/alu.sv
 }
 
-mem()
+MEM()
 {
     echo "Not implemented yet"
 }
 
-wrb()
+WB()
 {
     ncverilog  -sv  -q  +nctimescale+1ns/10ps mux_tb.sv       ../src/mux.sv
 }
