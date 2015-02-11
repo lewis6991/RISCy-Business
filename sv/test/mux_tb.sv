7cf4ac6d6268e34af8d0d2377723d86fe1ff0060
Dominic Murphy <dm16g11@soton.ac.uk>
2015-02-11 16:03:03 +0000

    Corrected timing of assertion calls.

--- sv/test/mux_tb.sv
+++ sv/test/mux_tb.sv
@@ -9,11 +9,11 @@ module mux_tb;
 
 timeunit 10ns; timeprecision 100ps;
 const int clk = 100;
-const int n = 32;
+parameter n = 32;
 
 logic Sel;
-logic [n:0] A, B;
-wire  [n:0] Y;
+logic [n-1:0] A, B;
+wire  [n-1:0] Y;
 
 mux mux0 ( 
     .Sel (Sel),
@@ -35,22 +35,23 @@ initial
 begin
     for (int i = 0 ; i < 4; i ++)
     begin
+        
         A = (i / 2) * 9673;
         B = (i % 2) * 3874;
 
         Sel = 1'b0;
+        #clk
         assert (Y == A) 
         else
             $display("ERROR: A = %d, B = %d, Sel = %b, Y = %d\n", A, B, Sel, Y);
-        #clk
     
         Sel = 1'b1;
+        #clk
         assert (Y == B) 
         else
             $display("ERROR: A = %d, B = %d, Sel = %b, Y = %d\n", A, B, Sel, Y);
-        #clk
     end
-    $finish
+    $finish;
 end
 
 endmodule
