f33f9cc97914d0dde5aff87d365e5ee5a003f7ad
Lewis M Russell <lmr1g10@hind.ecs.soton.ac.uk>
2015-02-12 16:07:47 +0000

    Added instructions for testcase 1 and wired up processor.

--- sv/test/processor_tb.sv
+++ sv/test/processor_tb.sv
@@ -14,17 +14,24 @@ parameter clk = 100;
 logic Clock  = 1'b0,
       nReset = 1'b0;
 
-logic [31:0] instData = 0;
-wire  [31:0] instAddr  ,
-             memAddr   ,
-             memRData  ,
-             memWData  ;
-wire         memReadEn ,
-             memWriteEn;
+logic [31:0] instrData = 0;
+wire  [31:0] instrAddr    ,
+             memAddr      ,
+             memRData     ,
+             memWData     ;
+wire         memReadEn    ,
+             memWriteEn   ;
 
 processor prcsr0 (
-    .Clock (Clock ),
-    .nReset(nReset)
+    .Clock    (Clock     ),
+    .nReset   (nReset    ),
+    .InstrMem (instrData ),
+    .InstrAddr(instrAddr ),
+    .MemData  (memRData  ),
+    .WriteData(memWData  ),
+    .MemAddr  (memAddr   ),
+    .MemWrite (memWriteEn),
+    .MemRead  (memReadEn )
 );
 
 memory memory0 (
@@ -41,13 +48,24 @@ memory memory0 (
 always
     #(clk/2) Clock = ~Clock;
 
+always @ (posedge Clock)
+    case(instrAddr)
+        16'h0000:  instrData <= #20 32'h3C011234;
+        16'h0004:  instrData <= #20 32'h34215678;
+        16'h0008:  instrData <= #20 32'h24020000;
+        16'h000c:  instrData <= #20 32'h00000000;
+        16'h0010:  instrData <= #20 32'h34420005;
+        16'h0014:  instrData <= #20 32'h70411802;
+        16'h0018:  instrData <= #20 32'h00000000;
+    endcase
+
 //Testing procedure
 initial
 begin
-    //Test nReset condition
-    #clk
+    #clk nReset = 1;
+    #200*clk
 
-    #clk
+    $finish;
 end
 
 endmodule
