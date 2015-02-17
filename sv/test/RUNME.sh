dd2834a367b1226acff3c4e9e4ecbea60d6faaca
Jack Barnes <jwb1g11@soton.ac.uk>
2015-02-17 13:49:36 +0000

    Added -plusargs functionality to -proc

--- sv/test/RUNME.sh
+++ sv/test/RUNME.sh
@@ -5,37 +5,53 @@
 # Description: Bash script for automated testing
 # Primary Author: Jack Barnes
 # Other Contributors: Dominic Murphy
-# Notes: 
+# Notes:
 #----------------------------------------
 
 main()
 {
     case $1 in
-        (-h|--help) usage;;
-        (-a|--all)  all;;
-        (-proc)     proc $2;;
-        (-if)       IF;;
-        (-id)       ID;;
-        (-ex)       EX;;
-        (-mem)      MEM;;
-        (-wb)       WB;;
-        (-s|--specific) specific $2;;
-        (-*)        echo "Unknown argument. Try --help for information";;
-        (*)         echo "No argument given. Try --help for information";;
+        (-h|--help)     usage;;
+        (-a|--all)      all;;
+        (-proc)         proc "$2" "$3";;
+        (-if)           IF;;
+        (-id)           ID;;
+        (-ex)           EX;;
+        (-mem)          MEM;;
+        (-wb)           WB;;
+        (-s|--specific) specific "$2" "$3";;
+        (-gui)          proc "$1";;
+        (-*)            echo "Unknown argument. Try --help for information";;
+        (*)             proc;;
     esac
 }
 
 usage()
 {
     echo "
-    -h,--help                 Displays this message
-    -a,--all                  Runs all test conditions
-    -if                       Runs the instruction fetch tests
-    -id                       Runs the instruction decode tests
-    -ex                       Runs the execute tests
-    -mem                      Runs the memory tests
-    -wb                       Runs the write back tests
-    -s, --specific 'module' Runs a singular test on 'module'
+    -h,--help         Displays this message
+    -a,--all          Runs all test conditions
+    -proc (-gui)      Runs the top level processor test
+    -if               Runs the instruction fetch tests
+    -id               Runs the instruction decode tests
+    -ex               Runs the execute tests
+    -mem              Runs the memory tests
+    -wb               Runs the write back tests
+    -s,--specific <module> (-gui)
+                      Runs a singular test on 'module'
+    -gui              Runs the top level processor test (With GUI)
+     NO ARGUMENT      Runs the top level processor test (Without GUI)
+
+    NOTE: Options in <> are required
+          Options in () are optional
+
+    -plusargs=\"\"      Include plusargs to any command to set arguments
+                      within the testbench itself. Full list below.
+                      Arguments can be chained by including a single
+                      comma between them. Left blank if not set
+    \"+START=10000\"   
+    \"+FINISH=10000\"
+    \"+FINISH=10000\"
     "
     exit 1
 }
@@ -53,14 +69,15 @@ all()
 proc()
 {
     echo "##### Testing the entire processor"
-    echo $1
     if [ "$1" = "-gui" ]
     then
         echo "Include GUI"
-        ncverilog +ncaccess+r -sv  -q +gui +incdir+../src/ +nctimescale+1ns/10ps processor_tb.sv ../src/*.sv
+        allvars=${2//"-plusargs="/}
+        ncverilog +ncaccess+r -sv -q +gui +incdir+../src/ $allvars +nctimescale+1ns/10ps processor_tb.sv ../src/*.sv
     else
         echo "Default"
-        ncverilog  -sv  -q +incdir+../src/ +nctimescale+1ns/10ps processor_tb.sv ../src/*.sv
+        allvars=${1//"-plusargs="/}
+        ncverilog +ncaccess+r -sv -q +incdir+../src/ $allvars +nctimescale+1ns/10ps processor_tb.sv ../src/*.sv
     fi
     echo "##### Processor done"
 }
@@ -74,7 +91,7 @@ IF()
     for i in ${MODULES[@]};
     do
         echo "##### Testing ${i}.sv"
-        ncverilog  -sv  -q  +nctimescale+1ns/10ps ${i}_tb.sv       ../src/${i}.sv
+        ncverilog -sv -q +nctimescale+1ns/10ps ${i}_tb.sv ../src/${i}.sv
         echo "##### ${i}.sv done"
     done
 }
@@ -88,7 +105,7 @@ ID()
     for i in ${MODULES[@]};
     do
         echo "Testing ${i}.sv"
-        ncverilog  -sv  -q  +nctimescale+1ns/10ps ${i}_tb.sv       ../src/${i}.sv
+        ncverilog -sv -q +nctimescale+1ns/10ps ${i}_tb.sv ../src/${i}.sv
         echo "##### ${i}.sv done"
     done
 }
@@ -102,7 +119,7 @@ EX()
     for i in ${MODULES[@]};
     do
         echo "Testing ${i}.sv"
-        ncverilog  -sv  -q  +nctimescale+1ns/10ps ${i}_tb.sv       ../src/${i}.sv
+        ncverilog -sv -q +nctimescale+1ns/10ps ${i}_tb.sv ../src/${i}.sv
         echo "##### ${i}.sv done"
     done
 }
@@ -122,25 +139,34 @@ WB()
     for i in ${MODULES[@]};
     do
         echo "Testing ${i}.sv"
-        ncverilog  -sv  -q  +nctimescale+1ns/10ps ${i}_tb.sv       ../src/${i}.sv
+        ncverilog -sv -q +nctimescale+1ns/10ps ${i}_tb.sv ../src/${i}.sv
         echo "##### ${i}.sv done"
     done
 }
 
-specific() #Warning - $1 in this function actually corresponds to $2 from the command line.
+#Note: $1 in this function actually corresponds to $2 from the command line.
+specific() 
 {
-    if [ $1 -eq ""]
+    if [ "$1" = "" ]
     then
         echo "No argument given. Try --help for information"
         exit
     fi
 
     echo "Testing specific module: ${1}.sv"
-    ncverilog  -sv  -q  +nctimescale+1ns/10ps ${1}_tb.sv       ../src/${1}.sv
+    if [ "$2" = "-gui" ]
+    then
+        echo "Include GUI"
+        ncverilog +ncaccess+r -sv -q +gui +nctimescale+1ns/10ps ${1}_tb.sv ../src/${1}.sv
+    else
+        echo "Default"
+        ncverilog -sv -q +nctimescale+1ns/10ps ${1}_tb.sv ../src/${1}.sv
+    fi
+
     echo "##### ${1}.sv done"
 }
 
-main $1 $2 $3
+main "$@"
 
 
 
