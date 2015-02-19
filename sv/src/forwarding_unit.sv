72f9daced6e7299c36b308f5d6005965d28e89e4
Dhanushan Raveendran <dr3g11@soton.ac.uk>
2015-02-19 17:27:34 +0000

    Started creating the forwarding unit

---
+++ sv/src/forwarding_unit.sv
@@ -0,0 +1,35 @@
+//------------------------------------------------------------------------------
+// File              : forwarding_unit.sv
+// Description       : Forwarding Unit to prevent Data Hazards
+// Primary Author    : Dhanushan Raveendran
+// Other Contributors: 
+// Notes             :
+//------------------------------------------------------------------------------
+
+module forwarding_unit(
+    input               RegWriteM,
+                        RegWriteW,
+    input        [31:0] RAddrM,
+                        RAddrW,
+                        RsAddr,
+	                RtAddr,
+    output logic [ 1:0] ForwardA,
+                        ForwardB 
+);
+
+always_comb
+    begin
+        if((RegWriteM) & (RAddrM != 0) & (RAddrM == RsAddr))
+            ForwardA = 2'b01;
+
+        if((RegWriteM) & (RAddrM != 0) & (RAddrM == RtAddr))
+            ForwardB = 1'b01;
+
+       if((RegWriteW) & (RAddrW != 0) & (RAddrW == RsAddr))
+            ForwardA = 2'b10;
+ 
+       if((RegWriteW) & (RAddrW != 0) & (RAddrW == RtAddr))
+            ForwardB = 1'b10;
+    end
+
+endmodule
