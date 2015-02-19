e3bbc2e89f36c04e85cbff934683d7a741cdddbd
lewis6991 <lewis6991@gmail.com>
2015-02-19 18:42:20 +0000

    Fixed another silly assertion.

--- sv/src/registers.sv
+++ sv/src/registers.sv
@@ -3,12 +3,12 @@
 // Description       : Program Counter
 // Primary Author    : Dominic Murphy
 // Other Contributors: Lewis Russell, Dhanushan Raveendran
-// Notes             : - 32 registers in place. 
+// Notes             : - 32 registers in place.
 //                     - Asynchronous read, synchronous write.
 //------------------------------------------------------------------------------
 
 module registers(
-    input               Clock   , 
+    input               Clock   ,
                         nReset  ,
                         RegWrite,
     input        [ 4:0] RdAddr  ,
@@ -20,30 +20,30 @@ module registers(
 );
 
     logic [31:0] data [31:0]; // 32 registers of 32 bit width (respectively).
-    
+
     // Synchronous write
     always_ff@(posedge Clock, negedge nReset)
         if (~nReset)
             /* for loop resets all registers to zero in the event of an asserted
              * nReset.*/
-            for (int k = 0; k < 32; k++) 
-                data[k] <= 32'd0;  
+            for (int k = 0; k < 32; k++)
+                data[k] <= 32'd0;
         else if (RegWrite)
             data[RdAddr] <= #20 RdData;
-    
+
     // Asynchronous read
 always_comb
 begin
     RsData = data[RsAddr];
     RtData = data[RtAddr];
-    
-    assert(data[0] == 0)
-    else
+
+    if (data[0] !== 32'dx)
     begin
-        //Test after time = 0
-        if (data[0] !== 32'dx)
+        assert(data[0] == 0)
+        else
+            //Test after time = 0
             $error("%dns: Register 0 is not zero. Register 0 should always be zero! %d", $time, data[0]);
     end
 end
-        
+
 endmodule
