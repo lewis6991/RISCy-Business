c394912d4854c4cda7949cba1f1d1dcf7f98f948
Dominic Murphy <dm16g11@soton.ac.uk>
2015-02-23 12:15:54 +0000

    Corrected errors reffering to example design

--- sv/syn/pnr_script.tcl
+++ sv/syn/pnr_script.tcl
@@ -22,7 +22,7 @@ fit
 addRing -spacing_bottom 2 -width_left 2 -width_bottom 2 -width_top 2 -spacing_top 2 -layer_bottom MET1 -stacked_via_top_layer MET4 -width_right 2 -around core -jog_distance 0.7 -offset_bottom 0.7 -layer_top MET1 -threshold 0.7 -offset_left 0.7 -spacing_right 2 -spacing_left 2 -offset_right 0.7 -offset_top 0.7 -layer_right MET2 -nets {gnd! vdd! } -stacked_via_bottom_layer MET1 -layer_left MET2
 sroute -connect { blockPin padPin padRing corePin floatingStripe } -layerChangeRange { 1 4 } -blockPinTarget { nearestRingStripe nearestTarget } -padPinPortConnect { allPort oneGeom } -checkAlignedSecondaryPin 1 -blockPin useLef -allowJogging 1 -crossoverViaBottomLayer 1 -allowLayerChange 1 -targetViaTopLayer 4 -crossoverViaTopLayer 4 -targetViaBottomLayer 1 -nets { gnd! vdd! }
 placeDesign -prePlaceOpt
-checkPlace qmults.checkPlace
+checkPlace processor.checkPlace
 setDrawView place
 optDesign -preCTS
 addCTSCellList {BUF2 INV2}
@@ -42,7 +42,7 @@ findCoreFillerCells
 addFiller -cell FILL1 FILL2 FILL5 FILL10 FILL25 -prefix FILLER -markFixed
 verifyConnectivity -type all -error 1000 -warning 50
 verifyGeometry
-streamOut qmultlayout.gds -mapFile streamOut.map -libName DesignLib -stripes 1 -units 1000 -mode ALL
+streamOut processorlayout.gds -mapFile streamOut.map -libName DesignLib -stripes 1 -units 1000 -mode ALL
 saveNetlist processor_net.v
 isExtractRCModeSignoff
 isExtractRCModeSignoff
