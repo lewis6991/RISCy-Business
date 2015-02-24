352180beee8a7a4953b28ea05d84c6b8f0839fae
jwb1g11 <jwb1g11@soton.ac.uk>
2015-02-24 16:11:42 +0000

    Update signextend_tb.sv

--- sv/test/signextend_tb.sv
+++ sv/test/signextend_tb.sv
@@ -11,16 +11,19 @@ timeunit 10ns; timeprecision 100ps;
 const int clk = 100;
 
 logic [15:0] In;
+logic Unsgnsel;
 wire  [31:0] Out;
 
 signextend sgnext0 (
-    .In  (In ),
-    .Out (Out)
+    .In       (In      ),
+    .Unsgnsel (Unsgnsel),
+    .Out      (Out     )
 );
 
 //Initial conditions
 initial
 begin
+    Unsgnsel = 1'b0;
     In  = 0;
 end
 
@@ -36,6 +39,21 @@ begin
     In = -3254;
     #clk
     assert (Out == {{16{In[15]}},In}) 
+    else
+        $display("ERROR: In[15] = %b, Sign extension = %b\n", In[15], Out[31:16]);
+    $finish;
+    
+    Unsgnsel = 1'b1;
+    
+    In = 743;
+    #clk
+    assert (Out == {{16{1'b0}}, In}) 
+    else
+        $display("ERROR: In[15] = %b, Sign extension = %b\n", In[15], Out[31:16]);
+
+    In = -3254;
+    #clk
+    assert (Out == {{16{1'b0}}, In}) 
     else
         $display("ERROR: In[15] = %b, Sign extension = %b\n", In[15], Out[31:16]);
     $finish;
