504a397f6523d477c1ab7c3be933785050a42e8c
jwb1g11 <jwb1g11@soton.ac.uk>
2015-02-24 16:15:24 +0000

    Update and rename sv/test to sv/test/ex_control_tb.sv

---
+++ sv/test/ex_control_tb.sv
@@ -0,0 +1,26 @@
+//----------------------------------------
+// File: ex_control_tb.sv
+// Description: Execute controller testbench
+// Primary Author: Jack Barnes
+// Other Contributors:
+// Notes: Full test coverage
+//----------------------------------------
+module ex_control_tb;
+
+timeunit 10ns; timeprecision 100ps;
+const int clk = 100;
+
+ex_control ex_control0 (
+);
+
+//Initial conditions
+initial
+begin
+end
+
+//Testing procedure
+initial
+begin
+end
+
+endmodule
