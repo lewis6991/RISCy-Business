07e46b6e697750100f8a64435cf91e1263610064
Dhanushan Raveendran <dr3g11@soton.ac.uk>
2015-02-24 11:33:48 +0000

    Added 3 input mux

---
+++ sv/src/3inmux.sv
@@ -0,0 +1,24 @@
+//------------------------------------------------------------------------------
+// File : 3inmux.sv
+// Description : 3 input multiplexers
+// Primary Author : Dhanushan Raveendran
+// Other Contributors:
+// Notes :
+//------------------------------------------------------------------------------
+
+module 3inmux #(parameter n = 32)(
+    input        [  1:0] Sel,
+    input        [n-1:0] A,
+                         B,
+                         C,
+    output logic [n-1:0] Y            
+);
+
+case(Sel)
+	0:       Y = A;
+	1:       Y = B;
+	2:       Y = C;
+	default: Y = A;
+endcase
+
+endmodule
