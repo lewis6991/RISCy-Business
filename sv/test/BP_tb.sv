8d9dbec08651530ecffed3daef6e4d29b0f4a2df
jwb1g11 <jwb1g11@soton.ac.uk>
2015-02-26 10:40:03 +0000

    Update and rename sv/test to sv/test/BP_tb.sv

---
+++ sv/test/BP_tb.sv
@@ -0,0 +1,26 @@
+//----------------------------------------
+// File: BP_tb.sv
+// Description: Branch prediction testbench
+// Primary Author: Jack Barnes
+// Other Contributors:
+// Notes: Full test coverage
+//----------------------------------------
+module BP_tb;
+
+timeunit 10ns; timeprecision 100ps;
+const int clk = 100;
+
+BP BP0 (
+);
+
+//Initial conditions
+initial
+begin
+end
+
+//Testing procedure
+initial
+begin
+end
+
+endmodule
