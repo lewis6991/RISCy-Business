b4cfabd23769e379475731b7a1a7b66302b1b62d
Dhanushan Raveendran <dr3g11@soton.ac.uk>
2015-02-26 16:48:06 +0000

    Fully working Data Forwarding Unit is Complete

--- sv/src/FU.sv
+++ sv/src/FU.sv
@@ -11,8 +11,12 @@ module FU(
                         RegWriteW,
     input        [ 4:0] RAddrM,
                         RAddrW,
+                        RsAddrE,
+                        RtAddrE,
                         RsAddr,
 	                RtAddr,
+    output logic        ForwardSrcA,
+                        ForwardSrcB,
     output logic [ 1:0] ForwardA,
                         ForwardB 
 );
@@ -21,18 +25,24 @@ always_comb
     begin
        ForwardA = 2'b00;
        ForwardB = 2'b00;
+       ForwardSrcA = 0;
+       ForwardSrcB = 0;
 
-       if((RegWriteM) & (RAddrM != 0) & (RAddrM == RsAddr))
+       if((RegWriteM) & (RAddrM != 0) & (RAddrM == RsAddrE))
             ForwardA = 2'b01;
-
-       if((RegWriteM) & (RAddrM != 0) & (RAddrM == RtAddr))
-            ForwardB = 2'b01;
-
-       if((RegWriteW) & (RAddrW != 0) & !(RegWriteM & (RAddrM != 0)) & (RAddrM != RsAddr) & (RAddrW == RsAddr))
-            ForwardA = 2'b10;
- 
-       if((RegWriteW) & (RAddrW != 0) & !(RegWriteM & (RAddrM != 0)) & (RAddrM != RtAddr) & (RAddrW == RtAddr))
+       else if((RegWriteW) & (RAddrW != 0) & (RAddrW == RsAddrE))
+            ForwardA = 2'b10;	
+		   
+       if((RegWriteM) & (RAddrM != 0) & (RAddrM == RtAddrE))
+            ForwardB = 2'b01; 
+       else if((RegWriteW) & (RAddrW != 0) & (RAddrW == RtAddrE))
             ForwardB = 2'b10;
+
+       if((RegWriteW) & (RAddrW != 0) & (RAddrW == RsAddr) & (((RegWriteM) & (RAddrM != 0) & (RAddrM == RsAddrE)) | ((RegWriteM) & (RAddrM != 0) & (RAddrM == RtAddrE)) | ((RegWriteW) & (RAddrW != 0) & (RAddrW == RsAddrE)) | ((RegWriteW) & (RAddrW != 0) & (RAddrW == RtAddrE))))
+            ForwardSrcA = 1;
+
+       if((RegWriteW) & (RAddrW != 0) & (RAddrW == RtAddr) & (((RegWriteM) & (RAddrM != 0) & (RAddrM == RsAddrE)) | ((RegWriteM) & (RAddrM != 0) & (RAddrM == RtAddrE)) | ((RegWriteW) & (RAddrW != 0) & (RAddrW == RsAddrE)) | ((RegWriteW) & (RAddrW != 0) & (RAddrW == RtAddrE))))
+            ForwardSrcB = 1;
     end
 
 endmodule
