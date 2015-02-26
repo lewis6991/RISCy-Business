9289b3dab0c8db0f2642504a5f4631c0064e605c
jwb1g11 <jwb1g11@soton.ac.uk>
2015-02-26 10:43:05 +0000

    Update and rename sv/src to sv/src/BP.sv

---
+++ sv/src/BP.sv
@@ -0,0 +1,12 @@
+/------------------------------------------------------------------------------
+// File              : BP.sv
+// Description       : Branch prediction unit
+// Primary Author    : -
+// Other Contributors: 
+// Notes             : Placeholder
+//------------------------------------------------------------------------------
+
+module BP(
+);
+
+endmodule
