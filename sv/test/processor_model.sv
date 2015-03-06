64069e70234f97afa690e2ea671f2d9f9707bf19
lewis6991 <lewis6991@gmail.com>
2015-03-06 15:16:37 +0000

    Implemented automatic checking of registers and program counter.

--- sv/test/processor_model.sv
+++ sv/test/processor_model.sv
@@ -6,36 +6,48 @@
 // Notes             :
 //------------------------------------------------------------------------------
 `include "op_definition.sv"
+`include "alu_definition.sv"
+`include "mul_definition.sv"
 module processor_model(
     input               Clock      ,
     input               nReset     ,
     input        [31:0] wData      ,
                         Instruction,
     output logic [31:0] rData      ,
-    input        [ 4:0] rAddr      ,
+    output       [15:0] InstAddr   ,
+    input        [ 4:0] rAddr,
+    output logic [15:0] pc
 );
+
+
+clocking delay @ (posedge Clock);
+    output pc;
+endclocking
 
 int register[0:31];
 
 logic [63:0] acc    ;
-logic [15:0] pc     ;
 logic [ 5:0] opcode ,
              func   ;
 logic [ 4:0] rs_addr,
              rt_addr,
              rd_addr,
              shamt  ;
-logic [15:0] imm    ;
+logic [15:0] imm    ,
+             offset ;
 logic [25:0] address;
 
-assign opcode  = instruction[31:26];
-assign rs_addr = instruction[25:21];
-assign rt_addr = instruction[20:16];
-assign rd_addr = instruction[15:11];
-assign shamt   = instruction[10: 6];
-assign func    = instruction[ 5: 0];
-assign imm     = instruction[15: 0];
-assign address = instruction[25: 0];
+assign opcode  = Instruction[31:26];
+assign rs_addr = Instruction[25:21];
+assign rt_addr = Instruction[20:16];
+assign rd_addr = Instruction[15:11];
+assign shamt   = Instruction[10: 6];
+assign func    = Instruction[ 5: 0];
+assign imm     = Instruction[15: 0];
+assign offset  = Instruction[15: 0];
+assign address = Instruction[25: 0];
+
+assign InstAddr = pc;
 
 `define rs register[rs_addr]
 `define rt register[rt_addr]
@@ -45,24 +57,32 @@ assign address = instruction[25: 0];
 // Register driver block
 always_ff @ (posedge Clock, negedge nReset)
     if (~nReset) begin
+        pc <= 0;
+        foreach(register[i])
+            register[i] <= 0;
     end
     else
     begin
-        pc <= #20 pc + 1;
+        rData <= register[rAddr];
+        pc <= pc + 4;
         case (opcode)
             `ADDI  ,
-            `ADDIU : `rt <= #20 `rs + imm ;
-            `LUI   : `rt <= #20 imm << 16;
-            `ANDI  : `rt <= #20 `rs & imm ;
-            `ORI   : `rt <= #20 `rs | imm ;
-            `XORI  : `rt <= #20 `rs ^ imm ;
+            `ADDIU : `rt <= `rs + imm ;
+            `LUI   : `rt <= imm << 16;
+            `ANDI  : `rt <= `rs & imm ;
+            `ORI   : `rt <= `rs | imm ;
+            `XORI  : `rt <= `rs ^ imm ;
             `SLTI  ,
-            `SLTIU : `rt <= #20 `rs < imm ? 32'b1 : 32'b0;
-            `BLTZAL:; //TODO
+            `SLTIU : `rt <= `rs < imm ? 32'b1 : 32'b0;
+            `BEQ   : if (`rs == `rt) delay.pc <= ##2 pc + (offset << 2);
+            `BGTZ  : if (`rs >  0  ) delay.pc <= ##2 pc + (offset << 2);
+            `BLEZ  : if (`rs <= 0  ) delay.pc <= ##2 pc + (offset << 2);
+            `BNE   : if (`rs != `rt) delay.pc <= ##2 pc + (offset << 2);
+            `J     : delay.pc <= ##2 {pc[31:28], 28'b0} + address;
             `JAL   :
             begin
-                pc <= #20 {pc[31:28] 28'b0} + address;
-                `ra <= #20 pc + 8;
+                delay.pc <= ##2 {pc[31:28], 28'b0} + address;
+                `ra <= pc + 8;
             end
             `LB    :; //TODO
             `LBU   :; //TODO
@@ -81,55 +101,88 @@ always_ff @ (posedge Clock, negedge nReset)
             `ALU   :
             begin
                 case (func)
-                    `SLL    : `rd <= #20 `rt <<  shamt;
-                    `SLLV   : `rd <= #20 `rt <<  `rs   ;
-                    `SRA    : `rd <= #20 `rt >>> shamt;
-                    `SRAV   : `rd <= #20 `rt >>> `rs   ;
-                    `SRL    : `rd <= #20 `rt >>  shamt;
-                    `SRLV   : `rd <= #20 `rt >>  `rs   ;
+                    `SLL    : `rd <= `rt <<  shamt;
+                    `SLLV   : `rd <= `rt <<  `rs   ;
+                    `SRA    : `rd <= `rt >>> shamt;
+                    `SRAV   : `rd <= `rt >>> `rs   ;
+                    `SRL    : `rd <= `rt >>  shamt;
+                    `SRLV   : `rd <= `rt >>  `rs   ;
                     `JALR   :; //TODO
-                    `MOVZ   : if (`rt == 0) `rd <= #20 `rs;
-                    `MOVN   : if (`rt != 0) `rd <= #20 `rs;
-                    `MFHI   : `rd <= #20 acc[31:16];
-                    `MFLO   : `rd <= #20 acc[15: 0];
-                    `MTHI   : acc[31:16] <= #20 `rs;
-                    `MTLO   : acc[15: 0] <= #20 `rs;
+                    `MOVZ   : if (`rt == 0) `rd <= `rs;
+                    `MOVN   : if (`rt != 0) `rd <= `rs;
+                    `MFHI   : `rd <= acc[63:32];
+                    `MFLO   : `rd <= acc[31: 0];
+                    `MTHI   : acc[63:32] <= `rs;
+                    `MTLO   : acc[31: 0] <= `rs;
                     `MULT   ,
-                    `MULTU  : acc <= #20 `rs*`rt;
-                    `ADD    : `rd <= #20 `rs + `rt;
-                    `ADDU   : `rd <= #20 `rs + `rt;
-                    `SUB    : `rd <= #20 `rs - `rt;
-                    `SUBU   : `rd <= #20 `rs - `rt;
-                    `AND    : `rd <= #20 `rs & `rt;
-                    `NOR    : `rd <= #20 ~(`rs | `rt);
-                    `OR     : `rd <= #20 `rs | `rt;
-                    `XOR    : `rd <= #20 `rs ^ `rt;
-                    `SLT    : `rd <= #20 `rs < `rt ? 32'b1 : 32'b0;
-                    `SLTU   : `rd <= #20 `rs < `rt ? 32'b1 : 32'b0;
-                    `JR     : pc <= #20 'rs;
+                    `MULTU  : acc <= `rs*`rt;
+                    `ADD    : `rd <= `rs + `rt;
+                    `ADDU   : `rd <= `rs + `rt;
+                    `SUB    : `rd <= `rs - `rt;
+                    `SUBU   : `rd <= `rs - `rt;
+                    `AND    : `rd <= `rs & `rt;
+                    `NOR    : `rd <= ~(`rs | `rt);
+                    `OR     : `rd <= `rs | `rt;
+                    `XOR    : `rd <= `rs ^ `rt;
+                    `SLT    : `rd <= `rs < `rt ? 32'b1 : 32'b0;
+                    `SLTU   : `rd <= `rs < `rt ? 32'b1 : 32'b0;
+                    `JR     : delay.pc <= ##2 `rs;
                     `JALR   :
                     begin
-                        `rd <= #20 pc + 8; pc <= #20 i`rs;
+                        `rd <= pc + 8;
+                        delay.pc <= ##2 `rs;
                     end
-                    default:;
+                    0:;//NOP
+                    default:
+                        assert(0)
+                        else
+                            $error("ERROR: This ALU instruction is not supported (func: 6'b%6b). ", func);
                 endcase
             end
 
             `BRANCH:
+            begin
+                case (rt_addr)
+                    `BGEZ  : if (`rs >= 0) delay.pc <= ##2 pc + (offset << 2);
+                    `BGEZAL:
+                    if (`rs >= 0)
+                    begin
+                        delay.pc <= ##2 pc + (offset << 2);
+                        `ra <= pc + 8;
+                    end
+                    `BLTZ  : if (`rs < 0) delay.pc <= ##2 pc + (offset << 2);
+                    `BLTZAL:
+                    if (`rs < 0)
+                    begin
+                        delay.pc <= ##2 pc + (offset << 2);
+                        `ra <= pc + 8;
+                    end
+                    default:
+                        assert(0)
+                        else
+                            $error("ERROR: This branch instruction is not supported(variant: 5'b%5b).", rt_addr);
+                endcase
+            end
             `MULL  :
             begin
                 case (func)
-                    `MADD   ,
-                    `MADDU  : acc <= #20 acc + `rs*`rt;
-                    `MSUB   ,
-                    `MSUBU  : acc <= #20 acc - `rs*`rt;
-                    `MUL    : `rd <= #20 `rs*`rt;
-                    `CLO    : `rd <= #20 count_leading_digit(`rs, 1);
-                    `CLZ    : `rd <= #20 count_leading_digit(`rs, 0);
-                    default:;
+                    `MADD  ,
+                    `MADDU : acc <= acc + `rs*`rt;
+                    `MSUB  ,
+                    `MSUBU : acc <= acc - `rs*`rt;
+                    `MUL   : `rd <= `rs*`rt;
+                    `CLO   : `rd <= count_leading_digit(`rs, 1);
+                    `CLZ   : `rd <= count_leading_digit(`rs, 0);
+                    default:
+                        assert(0)
+                        else
+                            $error("ERROR: This MULL insturction is not supported(func: 6'b%6b).", func);
                 endcase
             end
-            default:;
+            default:
+                assert(0)
+                else
+                    $error("ERROR: Thisop code is not supported(opcode: 6'b%6b).", opcode);
         endcase
     end
 
