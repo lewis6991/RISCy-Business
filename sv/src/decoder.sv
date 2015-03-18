a2fd37a63ba0625dcc8f7aff5cc202d953cdcb5a
Dhanushan Raveendran <dr3g11@soton.ac.uk>
2015-03-18 11:45:38 +0000

    Branch now takes unsign extended immediate value

--- sv/src/decoder.sv
+++ sv/src/decoder.sv
@@ -95,6 +95,7 @@ module decoder(
                     `BGEZ,
                     `BLTZ:
                     begin
+                        Unsgnsel = 1'b1    ;
                         Func     = BraCode ;
                         Branch   = 1'b1    ;
                         BRASrc   = 1'b1    ;
@@ -102,6 +103,7 @@ module decoder(
 
                     `BGEZAL, `BLTZAL:
                     begin
+                        Unsgnsel = 1'b1    ;
                         RegDst   = 2'b10   ;
                         Func     = BraCode ;
                         BRASrc   = 1'b1    ;
@@ -217,6 +219,7 @@ module decoder(
 
             `BLEZ, `BGTZ, `BEQ, `BNE:
             begin
+                Unsgnsel = 1'b1  ;
                 Func     = OpCode;
                 Branch   = 1'b1  ;
                 BRASrc   = 1'b1  ;
